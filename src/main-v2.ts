83ca929e24b504d3410982db7061d4561b92985b
Lemur <lemur@pop-os.localdomain>
2023-05-11 07:41:55 -0400

    Use Coinbase API to execute Buy & Sell Orders

--- src/main-v2.ts
+++ src/main-v2.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import CryptoJS from "crypto-js";
 
 import { getMovingAveragesWithPrice } from "./mongo";
 import { Candle } from "./types";
@@ -27,22 +28,31 @@ const main = async () => {
 			isPositionOpened == false
 		) {
 			// Open a position
-			const orderResponse = await sendBuyOrder(); // async fetch( "POST", { "order":"market", .... })
+			const orderResponse = await sendBuyOrder();
 			const order = await orderResponse.json();
 			// if (order.status == "200" || order.status == "201") {
 			if (order.data.success === true) {
 				isPositionOpened = true;
+				const assetAmount = 100.0 / price;
+
+				// localStorage.setItem(`assetAmount-BTC`, assetAmount);
+				// localStorage.setItem(`entryPrice-BTC`, price);
 			}
 		} else if (
 			(averages.ma7 < averages.ma21 || averages.ma7 < averages.ma55) &&
 			isPositionOpened == true
 		) {
 			// Close a position
-			const orderResponse = await sendSellOrder(); // async fetch( "POST", { "order":"market", .... })
+			const base_size = assetAmount * (price / entryPrice);
+
+			const orderResponse = await sendSellOrder(base_size);
 			const order = await orderResponse.json();
 			// if (orderResponse.status == "200" || orderResponse.status == "201") {
 			if (order.data.success === true) {
 				isPositionOpened = false;
+
+				// localStorage.setItem(`assetAmount-BTC`, null);
+				// localStorage.setItem(`entryPrice-BTC`, null);
 			}
 		}
 	} else {
@@ -69,8 +79,101 @@ const fetchPrice: Promise<number> = async () => {
 	}, 60000);
 };
 
-const sendBuyOrder = async () => {};
+const createSignature = (inputString: string, secret: string) => {
+	const hash = CryptoJS.HmacSHA256(inputString, secret);
+	return hash.toString();
+};
 
-const sendSellOrder = async () => {};
+const sendBuyOrder = async () => {
+	if (api_key === "") {
+		throw new Error("No API key found.");
+	}
+
+	if (api_secret === "") {
+		throw new Error("No API key secret found.");
+	}
+
+	const timestamp = Math.floor(Date.now() / 1000).toString();
+	const method = "POST";
+	const requestPath = "/api/v3/brokerage/orders";
+	const body = JSON.stringify({
+		client_order_id: Date.now().toString(),
+		product_id: "BTC-USD",
+		side: "BUY",
+		order_configuration: {
+			market_market_ioc: {
+				quote_size: "100",
+			},
+		},
+	});
+
+	const signature = createSignature(
+		timestamp + method + requestPath + body,
+		api_secret
+	);
+
+	const buyResponse = await fetch(
+		// `https://api.coinbase.com/api/v3/brokerage/orders`,
+		`https://api.coinbase.com${requestPath}`,
+		{
+			method,
+			headers: {
+				"CB-ACCESS-KEY": api_key,
+				"CB-ACCESS-TIMESTAMP": timestamp,
+				"CB-ACCESS-SIGN": signature,
+				"Content-Type": "application/json",
+			},
+			body,
+		}
+	);
+
+	return buyResponse;
+};
+
+const sendSellOrder = async (base_size: number) => {
+	if (api_key === "") {
+		throw new Error("No API key found.");
+	}
+
+	if (api_secret === "") {
+		throw new Error("No API key secret found.");
+	}
+
+	const timestamp = Math.floor(Date.now() / 1000).toString();
+	const method = "POST";
+	const requestPath = "/api/v3/brokerage/orders";
+	const body = JSON.stringify({
+		client_order_id: Date.now().toString(),
+		product_id: "BTC-USD",
+		side: "SELL",
+		order_configuration: {
+			market_market_ioc: {
+				base_size: base_size.toString(),
+			},
+		},
+	});
+
+	const signature = createSignature(
+		timestamp + method + requestPath + body,
+		api_secret
+	);
+
+	const sellResponse = await fetch(
+		// `https://api.coinbase.com/api/v3/brokerage/orders`,
+		`https://api.coinbase.com${requestPath}`,
+		{
+			method,
+			headers: {
+				"CB-ACCESS-KEY": api_key,
+				"CB-ACCESS-TIMESTAMP": timestamp,
+				"CB-ACCESS-SIGN": signature,
+				"Content-Type": "application/json",
+			},
+			body,
+		}
+	);
+
+	return sellResponse;
+};
 
 main();
